455610b91ab399c6562a316cc487e32040a71ebc
Nicholas <nicholaspriv@gmail.com>
2018-11-04 01:25:28 -0700

    Linked pages together

--- search_script.js
+++ search_script.js
@@ -18,22 +18,118 @@ function search() {
 	    } 
 	  }
 	}
-	function send() {
-	    var queryString = ""; // string with information to be sent to the next page
-	    var x = document.getElementById("frm1"); // x is the form
-	    for (var i = 0; i < x.length; i++) {
-	    	if (x.elements[i].type == "checkbox" && x.elements[i].checked)
-	    		queryString += "?" + x.elements[i].value; // send name and value from form
-	    }
-	    console.log("queryString: " + queryString);
-	    window.location.href = "search_response.html" + queryString;
-		}
-	function update() {
-		var selected = "Selected: ";
-		var x = document.getElementById("frm1"); // x is the form
-	    for (var i = 0; i < x.length; i++) {
-	    	if (x.elements[i].type == "checkbox" && x.elements[i].checked)
-	    		selected += x.elements[i].value + " "; // send name and value from form
-	    }
-	    document.getElementById("selected").innerHTML = selected;
+function send() {
+    var queryString = ""; // string with information to be sent to the next page
+    var x = document.getElementById("frm1"); // x is the form
+    for (var i = 0; i < x.length; i++) {
+    	if (x.elements[i].type == "checkbox" && x.elements[i].checked)
+    		queryString += "?" + x.elements[i].value; // send name and value from form
+    }
+    console.log("queryString: " + queryString);
+    window.location.href = "NegativeTraitsSelection.html" + queryString;
 	}
+function includes(array,trait) {
+	for (var i=0; i<array.length; i++) {
+		if (array[i] == trait)
+			return true;
+	}
+	return false;
+}
+function selectInitialTraits() { // For NegativeTraitsSelection.html
+	var queryString = "";
+	var x = document.getElementById("frm1"); // x is the form
+	var selectedTraits = []; // included traits (so as to not include a trait more than once)
+	for (var i = 0; i < x.length; i++) {
+		if (x.elements[i].type == "checkbox" && x.elements[i].checked && !includes(selectedTraits,x.elements[i].name)) {
+			queryString += "?" + x.elements[i].name;
+			selectedTraits.push(x.elements[i].name);
+		}
+	}
+	window.location.href = "MostImportantTraitsSelection.html" + queryString;
+}
+function selectAdditionalTraits() { // For MostImportantTraitsSelection.html
+	var queryString = decodeURIComponent(window.location.search);
+	var selectedTraits = queryString.split("?");
+	var x = document.getElementById("frm1"); // x is the form
+	for (var i=0; i<x.length; i++) {
+		if (x.elements[i].type == "checkbox" && x.elements[i].checked && !includes(selectedTraits,x.elements[i].name)) {
+			queryString += "?" + x.elements[i].name;
+			selectedTraits.push(x.elements[i].name);
+		}
+	}
+	window.location.href = "preference_selection.html" + queryString;
+}
+function displaySelectedBreeds() { // For NegativeTraitsSelection.html
+	var breedPhotos = new Map();
+	breedPhotos.set("Beagle","http://www.dogbreedchart.com/img/beagle.jpg");
+	breedPhotos.set("Chihuahua","http://www.dogbreedchart.com/img/chihuahua.jpg");
+	breedPhotos.set("Golden Retriever","http://www.dogbreedchart.com/img/golden-retriever.jpg");
+	breedPhotos.set("Pug","http://www.dogbreedchart.com/img/pug.jpg");
+
+	var queryString = decodeURIComponent(window.location.search);
+	var queries = queryString.split("?");
+	var breedTraits = "";
+	for (var i=1; i<queries.length; i++) {
+		breedTraits += (
+			"<u><em><strong><font size=\"20\">" + queries[i] + "</font></strong></em></u>   <br>" +
+			"<img src=" + breedPhotos.get(queries[i]) + " height=112 >" +
+        "<p1>" + 
+            "<ul>" +
+                "<li>Barking level: 5  <input type=\"checkbox\" name=\"barking\" > </li>" +
+                "<li>Activity level: 5  <input type=\"checkbox\" name=\"energy\" > </li>" +
+                "<li>Intelligence: 5  <input type=\"checkbox\" name=\"intelligence\");\"> </li>" +
+                "<li>Shedding: 5  <input type=\"checkbox\" name=\"shedding\"> </li>" +
+                "<li>Good with kids: 3  <input type=\"checkbox\" name=\"kids\" > </li>" +
+                "<li>Cuddliness: 3  <input type=\"checkbox\" name=\"cuddle\" > </li>" +
+                "<li>Temperament: 4  <input type=\"checkbox\" name=\"temperament\"> </li>" +
+                "<li>Time commitment: 4  <input type=\"checkbox\" name=\"time\" > </li>" +
+                "<li>Easy to train: 5  <input type=\"checkbox\" name=\"training\"> </li>" +
+                "<li>Size: 3  <input type=\"checkbox\" name=\"size\" > </li>" +
+                "<li>Health: 4  <input type=\"checkbox\" name=\"health\"> </li>" +                  
+            "</ul>" +
+        "</p1>"
+        );
+        document.getElementById("info").innerHTML = breedTraits;
+	}
+}
+function update() { // Updates the search on breed_search.html
+	var selected = "Selected: ";
+	var x = document.getElementById("frm1"); // x is the form
+    for (var i = 0; i < x.length; i++) {
+    	if (x.elements[i].type == "checkbox" && x.elements[i].checked)
+    		selected += x.elements[i].value + " "; // send name and value from form
+    }
+    document.getElementById("selected").innerHTML = selected;
+}
+function displayQuestionnaire() { // Generates html on preference_selection.html
+	var queryString = decodeURIComponent(window.location.search);
+	var queries = queryString.split("?");
+
+	var questionnaire = "";
+
+	for (var i=0; i<queries.length; i++) {
+		if (queries[i] == "energy") {
+			questionnaire += "<p>At its peak activity level, my dog should be able to... </p>" + 
+			  "<input type=\"radio\" name=\"energy\" value=\"5\"> Run and hike with me<br>" + 
+			  "<input type=\"radio\" name=\"energy\" value=\"4\"> Walk and play fetch<br>" +
+			  "<input type=\"radio\" name=\"energy\" value=\"3\"> Play occasional tug of war<br>" + 
+			  "<input type=\"radio\" name=\"energy\" value=\"2\"> Lay around for the most part, with the occoasional short walk<br>" +
+			  "<input type=\"radio\" name=\"energy\" value=\"1\"> Play on its own with minimal activity on my end<br><br>";
+		}
+		else if (queries[i] == "intelligence") {
+			questionnaire += "<p>What do you want your dog's intelligence level to be on a scale of 1 to 5, with 5 being most intelligent? </p>" +
+			  "<input type=\"radio\" name=\"Intelligence\" value=\"1\"> 1<br>" +
+			  "<input type=\"radio\" name=\"Intelligence\" value=\"2\"> 2<br>" +
+			  "<input type=\"radio\" name=\"Intelligence\" value=\"3\"> 3<br>" +
+			  "<input type=\"radio\" name=\"Intelligence\" value=\"4\"> 4<br>" +
+			  "<input type=\"radio\" name=\"Intelligence\" value=\"5\"> 5<br><br>";
+		}
+		else if (queries[i] == "barking") {
+			questionnaire += "<p>What do you want your dog's noise/barking level to be like?</p>" +
+		  "<input type=\"radio\" name=\"barking\" value=\"5\"> A guard dog with a strong bark<br>" +
+		  "<input type\"radio\" name=\"barking\" value=\"\"> No barking<br>" +
+		  "<input type=\"radio\" name=\"barking\" value=\"3\"> No preference<br><br>";
+		}
+	}
+
+}
