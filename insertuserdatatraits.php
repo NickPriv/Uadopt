34cb506157326db25e33f13aeae1d67e41a6d612
Steve Tointon <steve@Steves-MacBook-Pro.local>
2018-11-16 21:51:46 -0800

    addded sequence
    added sequence to atomatically increment the id column in the table

--- insertuserdatatraits.php
+++ insertuserdatatraits.php
@@ -179,6 +179,7 @@ function insertToDB($userInfo){
     echo "got into the insertto db";
     //sql statement that will read into the database
 	$sqlStatement = oci_parse($conn, "INSERT INTO Adopter Values(100, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, :barking, :energy, :intelligence, :shedding, :kids, :cuddle, :temperament, :timeForDog, :training, :health, :b_size) ");
+	$sqlStatement = oci_parse($conn, "INSERT INTO Adopter Values(sequence_1.nextval, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, :barking, :energy, :intelligence, :shedding, :kids, :cuddle, :temperament, :timeForDog, :training, :health, :b_size) ");
     /*binding each variable to the sql statement above with the php variable in the user info array */
     /*barking, energy, intelligence, sheddding, kids, cuddle, temperament, time, training, health, size are temp variables that must match with what is in the  sql statement above */
     oci_bind_by_name($sqlStatement, ':barking', $userInfo[0]);
--- tables.sql
+++ tables.sql
@@ -56,7 +56,21 @@ CREATE TABLE Match
 	Foreign Key(petID) REFERENCES Pet(petID)
 );
 
-INSERT INTO Pet values (1, 'Australian Shepherd', 4, 5, 5, 3, 4, 5, 4, 4, 5, 2, 3,
+CREATE SEQUENCE sequence_1
+start with 1
+increment by 1
+minvalue 0
+maxvalue 1000
+cycle;
+
+CREATE SEQUENCE sequence_2
+start with 1
+increment by 1
+minvalue 0
+maxvalue 1000
+cycle;
+
+INSERT INTO Pet values (sequence_2.nextval, 'Australian Shepherd', 4, 5, 5, 3, 4, 5, 4, 4, 5, 2, 3,
  'Australian Shepherds very energetic dogs. They are quite variable in temperament, 
  but these intelligent dogs can be a fun and exciting addition to your household. 
  Their energy is charming, but they do require a substantial amount of physical activity 
@@ -66,13 +80,13 @@ INSERT INTO Pet values (1, 'Australian Shepherd', 4, 5, 5, 3, 4, 5, 4, 4, 5, 2, 
 
 
 
-INSERT INTO Pet values (2, 'Beagle', 5, 4, 4, 3, 5, 4, 5, 2, 1, 1, 2, 
+INSERT INTO Pet values (sequence_2.nextval, 'Beagle', 5, 4, 4, 3, 5, 4, 5, 2, 1, 1, 2, 
 	'Beagles are active, playful and friendly dogs. They are great with kids and a 
 	convenient small size. But, don’t let their smaller size fool you, beagles still 
 	equire a moderate amount of exercise or an open yard to run around in. Beagles can 
 	be hard to train, so you must be diligent in training when they are puppies.');
 
-INSERT INTO Adopter values ('1', 'steve', 22, 1, 2, 2, 5, 2, 5, 5, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO Adopter values (sequence_1.nextval, 'steve', 22, 1, 2, 2, 5, 2, 5, 5, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 
 
