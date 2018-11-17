fe1f966ef710518562038ba654a4ae09bb63658e
Steve Tointon <steve@Steves-MacBook-Pro.local>
2018-11-16 22:06:48 -0800

    Update insertuserdatatraits.php
    added the header location to direct the browser to go to the html file for the user

--- insertuserdatatraits.php
+++ insertuserdatatraits.php
@@ -7,14 +7,13 @@
 //header("Location: https://uadopt.netlify.com/preference_selection.html");
 
 echo "does it get to this page after the presference selection";
-echo "this should come up";
 connectToDB();
 enterUser();
+header('Location: https://uadopt.netlify.com/calculate.html');
 
 
 function connectToDB(){
 
-        echo "<p>this is in the connectToDB funciton</p>";
         $cfg = parse_ini_file('setup.ini');
         $conn = oci_connect($cfg['db_user'], $cfg['db_pass'],$cfg['db_path']);
         if(!$conn){
@@ -26,7 +25,6 @@ function connectToDB(){
 
 function checkEmpty($array)
 {
-    echo "<p>this is in the check empty </p>";
 	foreach ($array as $value) {
 		if(empty($value)){
 			return false;
@@ -36,7 +34,6 @@ function checkEmpty($array)
 }
 
 function prepareInput($inputData){
-    echo "<p>this is in the prepareInput </p>";
     $inputData = trim($inputData);
     $inputData  = htmlspecialchars($inputData);
     return $inputData;
@@ -44,14 +41,11 @@ function prepareInput($inputData){
 
 function enterUser(){
 
-    echo "<p>this is in the enter user </p>";
-    print_r($_SERVER["REQUEST_METHOD"]);
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {  
      // collect input data of the 11 traits for user preferences barking, energy, 
      // intelligence, sheddding, kids, cuddle, temperament, time, training, health,
     //size
 	 // the varable name is single quotes must match the name from the html file 
-    echo "<p>before the if loop</p>";
     //$variableFromHtml = array("barking", "energy", "intelligence", "sheddding", "kids", "cuddle", "temperament", "time", "training", "health", "size");
     //print_r($_POST[$variableFromHtml]);
     if (!isset($_POST['barking']))
@@ -143,11 +137,6 @@ function enterUser(){
         $size= $_POST['size'];
         }
 
-    print_r($barking);
-    print_r($energy);
-    print_r($size);
-
-    echo "after the if loop";
      $userInfo = array(
      			$barking,
      			$energy,
@@ -160,13 +149,11 @@ function enterUser(){
                 $training,
                 $health,
                 $size);
-     echo "<p>this after the array part</p>";
      if(checkEmpty($userInfo)){
      	foreach ($userInfo as $attribute) {
      		prepareInput($attribute);
      	  }
      	insertToDB($userInfo);
-    echo "<p>this is after the data is suposed to be passed </p>";
         }
      else{
      	echo "Not all the required fields were completed";
@@ -176,9 +163,7 @@ function enterUser(){
 
 function insertToDB($userInfo){
 	$conn = connectToDB();
-    echo "got into the insertto db";
     //sql statement that will read into the database
-	$sqlStatement = oci_parse($conn, "INSERT INTO Adopter Values(100, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, :barking, :energy, :intelligence, :shedding, :kids, :cuddle, :temperament, :timeForDog, :training, :health, :b_size) ");
 	$sqlStatement = oci_parse($conn, "INSERT INTO Adopter Values(sequence_1.nextval, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, :barking, :energy, :intelligence, :shedding, :kids, :cuddle, :temperament, :timeForDog, :training, :health, :b_size) ");
     /*binding each variable to the sql statement above with the php variable in the user info array */
     /*barking, energy, intelligence, sheddding, kids, cuddle, temperament, time, training, health, size are temp variables that must match with what is in the  sql statement above */
