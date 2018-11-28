78853afd9fd44aa10878d826452ab4159cb3bbcf
Steve Tointon <steve@Steves-MacBook-Pro.local>
2018-11-28 02:53:08 -0800

    changed the form location

--- negative_traits_selection.php
+++ negative_traits_selection.php
@@ -5,16 +5,16 @@
         <link href="overall_style.css" rel="stylesheet" type="text/css">
         <link href="logo.css" rel="stylesheet" type="text/css">
 	<link href="https://fonts.googleapis.com/css?family=Poppins:100,300,500" rel="stylesheet">
-        <script type="text/javascript" src="./search_script.js"></script>
+        <script type="text/javascript" src="https://uadopt.netlify.com/search_script.js"></script>
     </head>
-    
-    <body onload="javascript:displaySelectedBreeds()">
         <a href="index.html">
  			<img src="uadoptLogo.png" id="logo">
 		</a>
         <div>
             <h2>The traits of this dog breed have been ranked on a scale of 1 to 5, with 1= low and 5= high.</h2>
             <h2>Please select any boxes with a rank that you would not prefer in your ideal dog.</h2>
+            <form id="frm1">
+            <span id="info">
             <?php
 
             $myurl = $_SERVER['REQUEST_URI'];
@@ -62,11 +62,10 @@
 
       ?>
 
-            <form id="frm1">
-            <span id="info"></span>     
+            </span>     
             </form>  
-        </div>
-        <button type="button" class="button" onclick="javascript:selectInitialTraits();">Next</button>
+        </div> 
+        <button type="button" class="button" onclick="selectInitialTraits()">Next</button>
     </body>
     
 </html>
